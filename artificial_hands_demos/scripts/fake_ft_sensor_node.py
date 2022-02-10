58c68b5485e231e397746840d9ce57d1ac8b3b65
penzottimattia <penzottimattia@gmail.com>
2022-02-10 15:37:43 +0100

    created a fake ft sensor

---
+++ artificial_hands_demos/scripts/fake_ft_sensor_node.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import WrenchStamped
+
+def main():
+  rospy.init_node('fake_ft_sensor_node')
+
+  ft = WrenchStamped()
+  ft.header.frame_id = 'ft_sensor_frame'
+  ft.wrench.force.x = 0
+  ft.wrench.force.y = 0
+  ft.wrench.force.z = 0
+  ft.wrench.torque.x = 0
+  ft.wrench.torque.y = 0
+  ft.wrench.torque.z = 0
+
+  pub = rospy.Publisher('ft_sensor',WrenchStamped,queue_size=1000)
+
+  rate = rospy.Rate(200)
+
+  while not rospy.is_shutdown():
+    pub.publish(ft)
+    rate.sleep()
+
+if __name__ == '__main__':
+  main()
