e56ddd642abdc6d46824cc4998771dd590afd5dd
penzottimattia <penzottimattia@gmail.com>
2022-02-14 14:14:27 +0100

    uploaded a new estimate_lift method

--- artificial_hands_py/src/artificial_hands_py/commanders.py
+++ artificial_hands_py/src/artificial_hands_py/commanders.py
@@ -6,13 +6,15 @@ from abc import ABC
 import numpy as np
 from threading import Thread
 
-import rospy
+import rospy, tf2_ros, tf2_geometry_msgs
+import tf.transformations as ts
 import moveit_commander
 import moveit_commander.conversions as cv
 from std_msgs.msg import Float64
+from std_srvs.srv import Empty
 from geometry_msgs.msg import PoseStamped
 from rqt_controller_manager.controller_manager import *
-from std_srvs.srv import Empty
+from cartesian_control_msgs.msg import *
 
 from artificial_hands_msgs.msg import *
 from artificial_hands_msgs.srv import WristCommand
@@ -129,17 +131,31 @@ class ArmCommander(moveit_commander.MoveGroupCommander):
   
   def __init__(self,ns=''):
     super().__init__("manipulator")
+    self.j_ctrl = ['pos_joint_traj_controller']
+    self.cart_ctrl = ['pose_based_cartesian_traj_controller']
     self.sw_ser = rospy.ServiceProxy('/controller_manager/switch_controller',SwitchController) 
-    self.pub = rospy.Publisher("/cartesian_motion_controller/command",PoseStamped,queue_size=1000)
+    self.traj_pub = rospy.Publisher('/pose_based_cartesian_traj_controller/follow_cartesian_trajectory/goal',FollowCartesianTrajectoryActionGoal,queue_size=1000)
+    self.set_pose_reference_frame('base')
+    self.set_end_effector_link('ft_sensor_frame')
     self.dt = 0.01
-    self.x_est = 0.0
-    self.y_est = 0.0
-    self.z_est = 0.0
+    self.t_p = 0.0
+    self.t_r = 0.0
+    self.x_p = 0.0
+    self.y_p = 0.0
+    self.z_p = 0.0
+    self.x_r = 0.0
+    self.y_r = 0.0
+    self.z_r = 0.0
 
-  def set_estimate_lift(self,x,y,z):
-    self.x_est = x
-    self.y_est = y
-    self.z_est = z
+  def set_estimate_lift(self,tp,tr,xp,yp,zp,xr,yr,zr):
+    self.t_p = tp
+    self.t_r = tr
+    self.x_p = xp
+    self.y_p = yp
+    self.z_p = zp
+    self.x_r = xr
+    self.y_r = yr
+    self.z_r = zr
 
   def harmonic(self,h,dt,ts):
     t = np.arange(0,ts+dt,dt)
@@ -158,52 +174,123 @@ class ArmCommander(moveit_commander.MoveGroupCommander):
     y = np.append(y_,np.flip(y_[:-1]))
     return y,t
 
-  def do_estimate_lift(self,T):
-    x_pos = self.biharmonic2(self.x_est,self.dt,T/2)[0]
-    y_pos = self.biharmonic2(self.y_est,self.dt,T/2)[0]
-    z_pos,t = self.harmonic(self.z_est,self.dt,T)
-    x_rot = np.zeros(len(x_pos))
-    y_rot = np.zeros(len(x_pos))
-    z_rot = np.zeros(len(x_pos))
+  def do_estimate_lift(self):
 
-    if not (len(x_pos) == len(y_pos) and len(x_pos) == len(z_pos)):
-      rospy.logerr("Error in cartesian points: inconsistent lengths.")
+    tf_buffer = tf2_ros.Buffer(rospy.Duration(100.0))
+    tf_listener = tf2_ros.TransformListener(tf_buffer)
+    
+    pose_ref = self.get_current_pose()
+    ref_to_base = tf_buffer.lookup_transform('base',pose_ref.header.frame_id,rospy.Time.now(),timeout=rospy.Duration(1))
+    pose = tf2_geometry_msgs.do_transform_pose(pose_ref,ref_to_base)
+    pose = cv.pose_to_list(pose.pose)
+    pos = pose[0:3]
+    quat = pose[3:]
+    rot = ts.euler_from_quaternion(quat)
+    
+    x_pos_arr,t_pos = self.biharmonic2(self.x_p,self.dt,self.t_p/2)
+    y_pos_arr = self.harmonic(self.y_p,self.dt,self.t_p)[0]
+    z_pos_arr = self.biharmonic2(self.z_p,self.dt,self.t_p/2)[0]
+    x_rot_arr,t_rot = self.biharmonic2(self.x_r,self.dt,self.t_r/2)
+    y_rot_arr = self.biharmonic2(self.y_r,self.dt,self.t_r/2)[0]
+    z_rot_arr = self.biharmonic2(self.z_r,self.dt,self.t_r/2)[0]
 
-    pos = cv.pose_to_list(self.get_current_pose().pose)[0:3]
+    if not (len(x_pos_arr) == len(y_pos_arr) and len(x_pos_arr) == len(z_pos_arr)):
+      if not (len(x_rot_arr) == len(y_rot_arr) and len(x_rot_arr) == len(z_rot_arr)):
+        rospy.logerr("Error in cartesian points: inconsistent lengths.")
+    
+    x_pos = np.ndarray.tolist(x_pos_arr)
+    y_pos = np.ndarray.tolist(y_pos_arr)
+    z_pos = np.ndarray.tolist(z_pos_arr)
 
-    x_pos = np.ndarray.tolist(pos[0] + x_pos)
-    y_pos = np.ndarray.tolist(pos[1] + y_pos)
-    z_pos = np.ndarray.tolist(pos[2] + z_pos)
+    x_rot = np.ndarray.tolist(x_rot_arr)
+    y_rot = np.ndarray.tolist(y_rot_arr)
+    z_rot = np.ndarray.tolist(z_rot_arr)
 
-    rot = self.get_current_rpy()
-    x_rot = np.ndarray.tolist(rot[0] + x_rot)
-    y_rot = np.ndarray.tolist(rot[1] + y_rot)
-    z_rot = np.ndarray.tolist(rot[2] + z_rot)
+    T = ts.quaternion_matrix(quat)
+    T[0:3,3] = pos
+
+    for i in range(0,len(x_rot)):
+      T_ = T.dot(ts.euler_matrix(x_rot[i],y_rot[i],z_rot[i]))
+      x_rot[i],y_rot[i],z_rot[i] = ts.euler_from_matrix(T_)
+
+    for i in range(0,len(x_pos)):
+      pos_ = [x_pos[i],y_pos[i],z_pos[i],1] 
+      x_pos[i],y_pos[i],z_pos[i] = T.dot((np.array(pos_)))[0:3]
+
+    toolc_to_ft = tf_buffer.lookup_transform('ft_sensor_frame','tool0_controller',rospy.Time.now(),timeout=rospy.Duration(1))
+    toolc_pose = cv.transform_to_list(toolc_to_ft.transform)
+    toolc_pos = toolc_pose[0:3]
+    toolc_quat = toolc_pose[3:]
+
+    x_pos_r = [pos[0]]*len(x_rot)
+    y_pos_r = [pos[1]]*len(x_rot)
+    z_pos_r = [pos[2]]*len(x_rot)
+    for i in range(0,len(x_rot)):
+      T0 = ts.euler_matrix(x_rot[i],y_rot[i],z_rot[i])
+      T0[0:3,3] = pos
+      T1 = ts.quaternion_matrix(toolc_quat)
+      T1[0:3,3] = toolc_pos
+      T01 = T0.dot(T1)
+      x_rot[i],y_rot[i],z_rot[i] = ts.euler_from_matrix(T01)
+      x_pos_r[i],y_pos_r[i],z_pos_r[i] = T01[0:3,3]
+    
+    x_rot_p = [rot[0]]*len(x_pos)
+    y_rot_p = [rot[1]]*len(x_pos)
+    z_rot_p = [rot[2]]*len(x_pos)
+    for i in range(0,len(x_pos)):
+      T0 = ts.quaternion_matrix(quat)
+      T0[0:3,3] = x_pos[i],y_pos[i],z_pos[i]
+      T1 = ts.quaternion_matrix(toolc_quat)
+      T1[0:3,3] = toolc_pos
+      T01 = T0.dot(T1)
+      x_pos[i],y_pos[i],z_pos[i] = T01[0:3,3]
+      x_rot_p[i],y_rot_p[i],z_rot_p[i] = ts.euler_from_matrix(T01)
+ 
+    e = 1
 
     cart_poses = []
-    for i in range(10,len(x_pos)-10):
-      pose = [x_pos[i],y_pos[i],z_pos[i],x_rot[i],y_rot[i],z_rot[i]]
+    for i in range(e,len(x_rot)-e):
+      pose = [x_pos_r[i],y_pos_r[i],z_pos_r[i],x_rot[i],y_rot[i],z_rot[i]]
       cart_poses.append(cv.list_to_pose(pose))
-
     (plan, fraction) = self.compute_cartesian_path(cart_poses,0.01,0.0)
-
     if fraction != 1:
-      rospy.logerr("ABORTED. Failed to compute a valid cartesian path (%.2f %%).",100*fraction)
+      rospy.logerr("ABORTED (rot). Failed to compute a valid path (%.2f %%)",fraction)
+      return
+    
+    cart_poses = []
+    for i in range(e,len(x_pos)-e):
+      pose = [x_pos[i],y_pos[i],z_pos[i],x_rot_p[i],y_rot_p[i],z_rot_p[i]]
+      cart_poses.append(cv.list_to_pose(pose))
+    (plan, fraction) = self.compute_cartesian_path(cart_poses,0.01,0.0)
+    if fraction != 1:
+      rospy.logerr("ABORTED (pos). Failed to compute a valid path (%.2f %%)",fraction)   
       return
 
-    self.sw_ser(['cartesian_motion_controller'],['pos_joint_traj_controller'],1,False,5) 
+    self.sw_ser(self.cart_ctrl,self.j_ctrl,1,False,5) 
 
-    i = 0
-    rate = rospy.Rate(1/self.dt)
-    while i < len(x_pos):
-      pose = cv.list_to_pose_stamped([x_pos[i],y_pos[i],z_pos[i],x_rot[i],y_rot[i],z_rot[i]],'world')
-      self.pub.publish(pose)
-      i += 1
-      rate.sleep()
-      
     sleep(1)
 
-    self.sw_ser(['pos_joint_traj_controller'],['cartesian_motion_controller'],1,False,5) 
+    cart_goal = FollowCartesianTrajectoryActionGoal()
+    for i in range(e,len(x_rot)-e):
+      cart_pnt = CartesianTrajectoryPoint() 
+      cart_pnt.pose = cv.list_to_pose([x_pos_r[i],y_pos_r[i],z_pos_r[i],x_rot[i],y_rot[i],z_rot[i]])
+      cart_pnt.time_from_start = rospy.Duration.from_sec(t_rot[i])
+      cart_goal.goal.trajectory.points.append(cart_pnt)
+    self.traj_pub.publish(cart_goal)
+
+    sleep(2*self.t_r)
+
+    cart_goal = FollowCartesianTrajectoryActionGoal()
+    for i in range(e,len(x_pos)-e):
+      cart_pnt = CartesianTrajectoryPoint() 
+      cart_pnt.pose = cv.list_to_pose([x_pos[i],y_pos[i],z_pos[i],x_rot_p[i],y_rot_p[i],z_rot_p[i]])
+      cart_pnt.time_from_start = rospy.Duration.from_sec(t_pos[i])
+      cart_goal.goal.trajectory.points.append(cart_pnt)
+    self.traj_pub.publish(cart_goal)
+
+    sleep(2*self.t_p)
+    
+    self.sw_ser(self.j_ctrl,self.cart_ctrl,1,False,5) 
 
 class RobotCommander(ABC):
   """ Inherite of this abstract class provides a common arm and hand movegroup
@@ -225,8 +312,8 @@ class RobotCommander(ABC):
   scene = moveit_commander.PlanningSceneInterface()
 
   arm = ArmCommander()
-  hand = MiaHandMoveitCommander()                                     # hand commander (useful now only for simulated hw)
-  # hand = MiaHandCommander("mia_hand")                                 # temp hand commander for real hw (since move_group doesn't work properly for mia hand)
+  # hand = MiaHandMoveitCommander()                                     # hand commander (useful now only for simulated hw)
+  hand = MiaHandCommander("mia_hand")                                 # temp hand commander for real hw (since move_group doesn't work properly for mia hand)
 
   def wristCommand(self,service_name):
     res = rospy.ServiceProxy(service_name,WristCommand)
