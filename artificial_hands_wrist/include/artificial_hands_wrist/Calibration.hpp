92af27b961c57f5af98f1fd734c61d5af48cde47
penzottimattia <penzottimattia@gmail.com>
2022-02-28 14:08:20 +0100

    added lin accel vector into equations

--- artificial_hands_wrist/include/artificial_hands_wrist/Calibration.hpp
+++ artificial_hands_wrist/include/artificial_hands_wrist/Calibration.hpp
@@ -25,20 +25,24 @@ namespace atk
        * @param f force vector
        * @param t torque vector
        * @param g gravity vector
+       * @param a acceleration vector
        */
-      void AddEquation(geometry_msgs::Vector3 f, geometry_msgs::Vector3 t, geometry_msgs::Vector3 g)
+      void AddEquation(geometry_msgs::Vector3 f, geometry_msgs::Vector3 t, geometry_msgs::Vector3 g, geometry_msgs::Vector3 a)
       {
         
         double gx = -g.x;
         double gy = -g.y;
         double gz = -g.z;
+        double ax = -a.x;
+        double ay = -a.y;
+        double az = -a.z;
 
-        double x[60] = {1., .0, .0, .0, .0, .0,    -gx,     .0,     .0,     .0,
-                        0., 1., .0, .0, .0, .0,    -gy,     .0,     .0,     .0,
-                        0., .0, 1., .0, .0, .0,    -gz,     .0,     .0,     .0, 
-                        0., .0, .0, 1., .0, .0,     .0,     .0,    -gz,     gy,
-                        0., .0, .0, .0, 1., .0,     .0,     gz,     .0,    -gx,
-                        0., .0, .0, .0, .0, 1.,     .0,    -gy,     gx,     .0};
+        double x[60] = {1., .0, .0, .0, .0, .0,    ax-gx,       .0,       .0,       .0,
+                        0., 1., .0, .0, .0, .0,    ay-gy,       .0,       .0,       .0,
+                        0., .0, 1., .0, .0, .0,    az-gz,       .0,       .0,       .0, 
+                        0., .0, .0, 1., .0, .0,       .0,       .0,    az-gz,    gy-ay,
+                        0., .0, .0, .0, 1., .0,       .0,    gz-az,       .0,    ax-gx,
+                        0., .0, .0, .0, .0, 1.,       .0,    ay-gy,    gx-ax,      .0};
 
         double y[6] = {f.x, f.y, f.z, t.x, t.y, t.z};
         
@@ -65,7 +69,7 @@ namespace atk
         for(int i = 0; i < 10; i++)cal_str << phi_[i] << " ";
         
         x_.clear();
-        y_.clear();
+        y_.clear();    
         return true;
       }
 
@@ -95,7 +99,6 @@ namespace atk
 
       std::vector<double> x_;
       std::vector<double> y_;
-      std::vector<double> V_;
       std::vector<double> phi_;
        
   };
