78866e0c674053fb598d6c40566821a393a56afc
penzottimattia <penzottimattia@gmail.com>
2022-02-28 12:00:38 +0100

    fixed some constant declaration

--- artificial_hands_demos/src/handover_release_demo/constants.py
+++ artificial_hands_demos/src/handover_release_demo/constants.py
@@ -2,31 +2,32 @@ from cmath import pi
 
 from geometry_msgs.msg import Pose
 
-joint_start = [pi/2,-1.28,2.09,-0.8,pi/2,.0]
-""" Hardcoded arm joint angles for start position"""
-
-joint_grasp = joint_start
+joint_home = [pi/2,-pi/2,pi/2,.0,pi/2,.0]
 """ Hardcoded arm joint angles for home position"""
 
-joint_reach = [.0,-pi/2,pi/2,.0,pi/2,.0]
+joint_grasp = [pi,-pi/2,pi/2,.0,pi/2,.0]
+""" Hardcoded arm joint angles for home position"""
+
+joint_start = joint_grasp
+""" Hardcoded arm joint angles for start reach position"""
+
+joint_reach = joint_home
 """ Hardcoded arm joint angles for reach position"""
 
-joint_home = joint_reach
-""" Hardcoded arm joint angles for home position"""
+joint_calib = []
+joint_calib = joint_home.copy()
+joint_calib[3] = -0.6
+joint_calib[5] = pi/4
+""" Hardcoded arm joint angles for reach position"""
 
-b = joint_home[0]
-s = joint_home[1]
-e = joint_home[2]
-w1 = joint_home[3]
-w2 = joint_home[4]
-w3 = joint_home[5]
-
-calibration_joints = [[b,s,e,w1,w2,w3-pi],
-                      [b,s,e,w1,w2,w3-pi/2],
-                      [b,s,e,w1,w2,w3+pi/2],
-                      [b,s,e,w1-pi/2,w2,w3+pi/2],
-                      [b,s,e,w1+pi/2,w2,w3+pi/2],
-                      [b,s,e,w1,w2,w3]]
+calibration_joints = []
+for c in range(0,6):
+  calibration_joints.append(joint_home.copy())
+calibration_joints[0][5] -= pi
+calibration_joints[1][5] -= pi/2
+calibration_joints[2][5] += pi/2
+calibration_joints[3][3] -= pi/2
+calibration_joints[4][3] += pi/2
 """ Hardcoded arm joint angles for fast calibration of force/torque """
 
 goal_time = 1.12
