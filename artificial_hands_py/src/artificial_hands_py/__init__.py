7f0ac64630f73a195d7660ff9f127b70ab18c212
penzottimattia <penzottimattia@gmail.com>
2022-03-07 18:27:13 +0100

    minor commit to py pkg

--- artificial_hands_py/src/artificial_hands_py/__init__.py
+++ artificial_hands_py/src/artificial_hands_py/__init__.py
@@ -1,1 +1,15 @@
 from artificial_hands_py_base import BaseFilter, WristFTDetection
+
+import sys, select, termios, tty
+
+key_settings = termios.tcgetattr(sys.stdin)
+
+def get_key(key_timeout):
+  tty.setraw(sys.stdin.fileno())
+  rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
+  if rlist:
+      key = sys.stdin.read(1)
+  else:
+      key = ''
+  termios.tcsetattr(sys.stdin, termios.TCSADRAIN, key_settings)
+  return key
--- artificial_hands_py/src/artificial_hands_py/robot_commander/__init__.py
+++ artificial_hands_py/src/artificial_hands_py/robot_commander/__init__.py
@@ -1,2 +1,14 @@
+import numpy as np
+from cmath import pi
+
+import rospy
+import tf2_ros, tf2_geometry_msgs, tf.transformations as ts
+
+from std_msgs.msg import Float64MultiArray
+from geometry_msgs.msg import Pose, PoseStamped, Twist, TwistStamped
+from trajectory_msgs.msg import JointTrajectory
+
+from artificial_hands_py.robot_commander.servo_commander_base import ServoCommanderBase
 from artificial_hands_py.robot_commander.harmonic_servo_commander import HarmonicServoCommander
 from artificial_hands_py.robot_commander.mia_hand_commander import MiaHandCommander
+
