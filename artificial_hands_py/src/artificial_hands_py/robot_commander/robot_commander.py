bf84dc16e62fca16feae2bc2d8fcbf18762f6207
penzottimattia <penzottimattia@gmail.com>
2022-05-27 16:40:36 +0200

    updated robot commander with new mia hand controllers

--- artificial_hands_py/src/artificial_hands_py/robot_commander/robot_commander.py
+++ artificial_hands_py/src/artificial_hands_py/robot_commander/robot_commander.py
@@ -1,6 +1,6 @@
 from abc import ABC
 
-from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import Float64MultiArray, Float64
 from geometry_msgs.msg import PoseStamped
 from trajectory_msgs.msg import JointTrajectory, MultiDOFJointTrajectory
 
@@ -28,6 +28,8 @@ class RobotCommander(ABC):
                 cart_eik_pos_ctrl : MultiDOFJointTrajectory,
                 cart_eik_vel_ctrl : MultiDOFJointTrajectory}
 
+    #TO DO Move this publishers within the ArmCommander class
+    
     cart_mot_pos_pub = PoseStampedPublisher(cart_mot_pos_ctrl+'/command')
     cart_mot_vel_pub = PoseStampedPublisher(cart_mot_vel_ctrl+'/command')
     cart_eik_pos_pub = CartesianMDOFPointPublisher(cart_eik_pos_ctrl+'/command')
@@ -37,9 +39,17 @@ class RobotCommander(ABC):
 
     mia_j_traj_ctrl = 'mia_hand_hw_vel_trajectory_controller'
     mia_j_vel_ctrl = 'mia_hand_joint_group_vel_controller'
-    
+    mia_j_index_pos_vel_ctrl = 'mia_hand_j_index_fle_pos_vel_controller'
+    mia_j_mrl_pos_vel_ctrl = 'mia_hand_j_mrl_fle_pos_vel_controller'
+    mia_j_thumb_pos_vel_ctrl = 'mia_hand_j_thumb_fle_pos_vel_controller'
+
     mia_ctrl_dict = {mia_j_traj_ctrl : JointTrajectory,
-                mia_j_vel_ctrl : Float64MultiArray}
+                mia_j_vel_ctrl : Float64MultiArray,
+                mia_j_index_pos_vel_ctrl : Float64,
+                mia_j_mrl_pos_vel_ctrl : Float64,
+                mia_j_thumb_pos_vel_ctrl : Float64}
+
+    hand = MiaHandCommander(ns='mia_hand',ctrl_dict=mia_ctrl_dict)
 
     self.hand = MiaHandCommander(ns='mia_hand',ctrl_dict=mia_ctrl_dict)
 
