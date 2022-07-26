01d11a6a8a72392e3e03a4240860f2b83bd1aa61
penzottimattia <penzottimattia@gmail.com>
2022-07-26 17:18:47 +0200

    improved commander and gui

--- artificial_hands_py/src/artificial_hands_py/robot_commander/mia_hand_commander.py
+++ artificial_hands_py/src/artificial_hands_py/robot_commander/mia_hand_commander.py
@@ -1,11 +1,14 @@
+from math import floor
 from threading import Thread, Lock
+from time import sleep
 
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory
-from std_msgs.msg import Float64MultiArray, Float64
+from std_msgs.msg import Float64MultiArray, Float64, Empty
 
 from artificial_hands_py.artificial_hands_py_base import singleton
 from artificial_hands_py.robot_commander.controller_manager_base import *
+from artificial_hands_py.cartesian_trajectory_generator.cartesian_trajectory_base import harmonic_pos
 
 @singleton
 class MiaHandCommander(ControllerManagerBase):
@@ -32,17 +35,22 @@ class MiaHandCommander(ControllerManagerBase):
     self.rate = rospy.Rate(20)
 
     self.j_pos = [.0,.0,.0]
+    self.j_vel = [.0,.0,.0]
+    self.j_acc = [.0,.0,.0]
 
     j_sub = rospy.Subscriber(ns+"/joint_states",JointState,self.joint_states_callback)
     j_pos_sub = rospy.Subscriber(ns+"/mia_hand_joint_group_pos_vel_controller/command",Float64MultiArray,self.j_pos_vel_callback)
-
-    self.switch_to_pos_vel_controllers()
+    j_traj_sub = rospy.Subscriber(ns+"/mia_hand_joint_group_traj_controller/command",Float64MultiArray,self.j_traj_callback)
+    j_stop_sub = rospy.Subscriber(ns+"/mia_hand_joint_group_stop_controller/command",Empty,self.j_stop_callback)
+    j_open_sub = rospy.Subscriber(ns+"/mia_hand_joint_group_open_controller/command",Float64,self.j_open_callback)
 
     self.j_index_pos = Float64()
     self.j_mrl_pos = Float64()
     self.j_thumb_pos = Float64()
 
-    self.j_index_pos.data = 0.4
+    rospy.wait_for_message(ns+"/joint_states",JointState)
+
+    self.switch_to_pos_vel_controllers()
 
     j_index_pos_thread = Thread(target=self.j_index_pos_vel_update)
     j_mrl_pos_thread = Thread(target=self.j_mrl_pos_vel_update)
@@ -59,37 +67,103 @@ class MiaHandCommander(ControllerManagerBase):
     self.j_thumb_pos.data = j_pos[2]
     self.lock.release()
   
+  def set_joint_target_positions(self, target : list, goal_time : float):
+
+    c_target = self.j_pos.copy()
+
+    delta_target = [y - x for (x,y) in zip(c_target,target)]
+
+    dt = self.rate.sleep_dur.to_sec()
+
+    c_max = floor(goal_time/dt) + 1
+    goal_time = c_max*dt
+
+    for c in range(1,c_max+1):
+
+      self.lock.acquire()
+
+      self.j_index_pos.data = c_target[0] + harmonic_pos(delta_target[0],c*dt,goal_time)
+      self.j_mrl_pos.data = c_target[1] + harmonic_pos(delta_target[1],c*dt,goal_time)
+      self.j_thumb_pos.data = c_target[2] + harmonic_pos(delta_target[2],c*dt,goal_time)
+
+      self.lock.release()
+
+      self.rate.sleep()
+  
   def switch_to_pos_vel_controllers(self):
+    self.j_index_pos.data = self.j_pos[0]
+    self.j_mrl_pos.data = self.j_pos[1]
+    self.j_thumb_pos.data = self.j_pos[2]
     self.start_controllers([self.mia_j_index_pos_vel_ctrl,
                             self.mia_j_mrl_pos_vel_ctrl,
                             self.mia_j_thumb_pos_vel_ctrl])
 
   def j_index_pos_vel_update(self):
     while not rospy.is_shutdown():
+      self.lock.acquire()
       self.controller_command(self.j_index_pos,ctrl=self.mia_j_index_pos_vel_ctrl)
+      self.lock.release()
       self.rate.sleep()
-  
+      
   def j_mrl_pos_vel_update(self):
     while not rospy.is_shutdown():
+      self.lock.acquire()
       self.controller_command(self.j_mrl_pos,ctrl=self.mia_j_mrl_pos_vel_ctrl)
+      self.lock.release()
       self.rate.sleep()
 
   def j_thumb_pos_vel_update(self):
     while not rospy.is_shutdown():
+      self.lock.acquire()
       self.controller_command(self.j_thumb_pos,ctrl=self.mia_j_thumb_pos_vel_ctrl)
-      self.rate.sleep()                          
-      
-  def stop(self,autoswitch=True):
+      self.lock.release()
+      self.rate.sleep()   
+                             
+  def stop(self):
+    self.lock.acquire()
     self.switch_to_controller(self.mia_j_vel_ctrl)
     j_vel = Float64MultiArray()
     j_vel.data = [.0,.0,.0]
     self.controller_command(j_vel)
+    rospy.sleep(rospy.Duration.from_sec(0.5))
+    self.switch_to_pos_vel_controllers()
+    self.lock.release()
+  
+  def open(self, vel : float = 0.5):
+
+    self.lock.acquire()
+    self.switch_to_controller(self.mia_j_vel_ctrl)
+    j_vel = Float64MultiArray()
+    j_vel.data = [-vel,-vel,-vel]
+    self.controller_command(j_vel)
+
+    dur_open = [abs(y - x)/vel for (x,y) in zip(self.j_pos,[0.3,0.7,0.0])]
+    class JointPauser:
+      def __init__(self, j_index : int, dur_open : list, j_vel : list) -> None:
+        pause_thread = Thread(target=self.pause,args=[j_index,dur_open,j_vel])
+        pause_thread.start()
+      
+      def pause(self, j_index : int, dur_open : list, j_vel : list):
+        rospy.sleep(rospy.Duration.from_sec(dur_open[j_index]))
+        j_vel[j_index] = 0
+
+    j_index_pauser = JointPauser(0,dur_open,j_vel.data)
+    j_mrl_pauser = JointPauser(1,dur_open,j_vel.data)
+    j_thumb_pauser = JointPauser(2,dur_open,j_vel.data)
+    while not all(v == 0 for v in j_vel.data):
+      self.controller_command(j_vel)
+      self.rate.sleep()
+
+    rospy.sleep(rospy.Duration.from_sec(0.5))
+    self.switch_to_pos_vel_controllers()
+    self.lock.release()
   
   def joint_states_callback(self,msg : JointState):
     robot_joints = msg.name
     c = 0
     for j in self.joint_names:
-      self.j_pos[c] = msg.position[robot_joints.index(j)]    
+      self.j_pos[c] = msg.position[robot_joints.index(j)] 
+      self.j_vel[c] = msg.velocity[robot_joints.index(j)]    
       c += 1  
 
   def j_pos_vel_callback(self,msg : Float64MultiArray):
@@ -98,12 +172,21 @@ class MiaHandCommander(ControllerManagerBase):
     self.j_mrl_pos.data = msg.data[1]
     self.j_thumb_pos.data = msg.data[2]
     self.lock.release()
+  
+  def j_traj_callback(self,msg : Float64MultiArray):
+    self.set_joint_target_positions(target=msg.data[0:3],goal_time=msg.data[3])
+  
+  def j_stop_callback(self,msg : Empty):
+    self.stop()
+
+  def j_open_callback(self,msg : Float64):
+    self.open(vel=msg.data)
 
 def main():
 
   rospy.init_node('mia_hand_commander_node')
 
-  hand = MiaHandCommander(ns='')
+  hand = MiaHandCommander(ns='mia_hand')
 
   rospy.loginfo('Mia hand commander ready!')
 
--- artificial_hands_py/src/artificial_hands_py/robot_commander/mia_hand_commander_gui.py
+++ artificial_hands_py/src/artificial_hands_py/robot_commander/mia_hand_commander_gui.py
@@ -1,6 +1,8 @@
 import sys
+from functools import partial
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
+from sympy import rad
 
 import rospy
 
@@ -46,17 +48,29 @@ class GraspGroupBox(QGroupBox):
 
   def __init__(self,parent : QWidget = None, title : str = None):
     super().__init__()
-    j_label,self.j_spin_box = new_param_item('ind',-1.4,1.4,0.01,False)
-    m_label,self.m_spin_box = new_param_item('thu',0,1.4,0.01,False)
-    t_label,self.t_spin_box = new_param_item('mrl',0,1.4,0.01,False)
+    ind_label,self.ind_spin_box = new_param_item('ind',-1.4,1.4,0.05,False)
+    mrl_label,self.mrl_spin_box = new_param_item('mrl',0,1.4,0.05,False)
+    thu_label,self.thu_spin_box = new_param_item('thu',0,1.4,0.05,False)
+    time_label,self.time_spin_box = new_param_item('time',0,2.0,0.2,False)
+
+    rad_label = QLabel("[rad]")
+    rad_label.setAlignment(Qt.AlignCenter | Qt.AlignLeft)
+    sec_label = QLabel("[s]")
+    sec_label.setAlignment(Qt.AlignCenter | Qt.AlignLeft)
 
     spin_box_layout = QHBoxLayout()
-    spin_box_layout.addWidget(j_label)
-    spin_box_layout.addWidget(self.j_spin_box)
-    spin_box_layout.addWidget(m_label)
-    spin_box_layout.addWidget(self.m_spin_box)
-    spin_box_layout.addWidget(t_label)
-    spin_box_layout.addWidget(self.t_spin_box)
+    spin_box_layout.addWidget(ind_label)
+    spin_box_layout.addWidget(self.ind_spin_box)
+    spin_box_layout.addWidget(rad_label)
+    spin_box_layout.addWidget(mrl_label)
+    spin_box_layout.addWidget(self.mrl_spin_box)
+    spin_box_layout.addWidget(rad_label)
+    spin_box_layout.addWidget(thu_label)
+    spin_box_layout.addWidget(self.thu_spin_box)
+    spin_box_layout.addWidget(rad_label)
+    spin_box_layout.addWidget(time_label)
+    spin_box_layout.addWidget(self.time_spin_box)
+    spin_box_layout.addWidget(sec_label)
 
     target_layout = QVBoxLayout()
     target_layout.addLayout(spin_box_layout)
@@ -81,16 +95,64 @@ class MiaHandCommanderGui(QWidget):
     self.joint_group_box.m_spin_box.valueChanged.connect(self.update_joint_positions)
     self.joint_group_box.t_spin_box.valueChanged.connect(self.update_joint_positions)
 
+    self.grasp_group_box = GraspGroupBox(title="Grasp")
+    self.grasp_group_box.ind_spin_box.setValue(0.56)
+    self.grasp_group_box.mrl_spin_box.setValue(1.00)
+    self.grasp_group_box.thu_spin_box.setValue(0.60)
+    self.grasp_group_box.time_spin_box.setValue(1.0)
+
+    self.close_push_button = QPushButton("Close")
+    self.stop_push_button = QPushButton("Stop")
+    self.open_push_button = QPushButton("Open")
+    
+    self.open_vel_label = QLabel("[rad/s]")
+    self.open_vel_spin_box = QDoubleSpinBox()
+    self.open_vel_spin_box.setValue(1.5)
+    self.open_vel_spin_box.setRange(0.25,3.0)
+    self.open_vel_spin_box.setSingleStep(0.25)
+
+    self.close_push_button.clicked.connect(self.set_grasp_joint_positions)
+    self.stop_push_button.clicked.connect(self.stop_joint_positions)
+    self.open_push_button.clicked.connect(self.zero_joint_positions)
+    self.grasp_group_box.clicked.connect(partial(self.joint_group_box.setChecked, False))
+    self.joint_group_box.clicked.connect(partial(self.grasp_group_box.setChecked, False))
+
+    self.buttons_layout = QHBoxLayout()
+    self.buttons_layout.addWidget(self.close_push_button)
+    self.buttons_layout.addWidget(self.stop_push_button)
+    self.buttons_layout.addWidget(self.open_push_button)
+    self.buttons_layout.addWidget(self.open_vel_spin_box)
+    self.buttons_layout.addWidget(self.open_vel_label)
+
     main_layout = QVBoxLayout()
     main_layout.addWidget(self.joint_group_box)
+    main_layout.addWidget(self.grasp_group_box)
+    main_layout.addLayout(self.buttons_layout)
 
     self.setLayout(main_layout)
+  
+  def stop_joint_positions(self):
+    self.joint_group_box.setChecked(False)
+    self.hand.stop()
+  
+  def zero_joint_positions(self):
+    self.joint_group_box.setChecked(False)
+    self.hand.open(self.open_vel_spin_box.value())
+    self.refresh_joint_positions()
 
   def refresh_joint_positions(self):
-    self.joint_group_box.i_spin_box.setValue(self.hand.j_pos[0])
-    self.joint_group_box.m_spin_box.setValue(self.hand.j_pos[1])
-    self.joint_group_box.t_spin_box.setValue(self.hand.j_pos[2])
-
+    if self.joint_group_box.isChecked():  
+      self.joint_group_box.i_spin_box.setValue(self.hand.j_pos[0])
+      self.joint_group_box.m_spin_box.setValue(self.hand.j_pos[1])
+      self.joint_group_box.t_spin_box.setValue(self.hand.j_pos[2])
+  
+  def set_grasp_joint_positions(self):
+    if self.grasp_group_box.isChecked(): 
+      if self.grasp_group_box.time_spin_box.value() > 0.5:
+        self.hand.set_joint_target_positions([self.grasp_group_box.ind_spin_box.value(),self.grasp_group_box.mrl_spin_box.value(),self.grasp_group_box.thu_spin_box.value()],self.grasp_group_box.time_spin_box.value())
+      else:
+        self.hand.set_joint_positions([self.grasp_group_box.ind_spin_box.value(),self.grasp_group_box.mrl_spin_box.value(),self.grasp_group_box.thu_spin_box.value()])
+       
   def update_joint_positions(self):
     self.hand.set_joint_positions([self.joint_group_box.i_spin_box.value(),self.joint_group_box.m_spin_box.value(),self.joint_group_box.t_spin_box.value()])    
 
